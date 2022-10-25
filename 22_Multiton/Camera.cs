46f932b2a5f0a0e96d2555dbc5c1097ab5a95d7e
AloKzMstf <mstf.gdik19@gmail.com>
2022-10-25 16:44:16 +0300

    Multiton

---
+++ 22_Multiton/Camera.cs
@@ -0,0 +1,26 @@
+﻿namespace _22_Multiton
+{
+    public class Camera
+    {
+        static Dictionary<string, Camera> _camera = new();
+
+        static object _lock = new object();
+        public Guid Id { get; set; }
+        private Camera()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public static Camera GetCamera(string brand)
+        {
+            lock (_lock)
+            {
+                if (!_camera.ContainsKey(brand))
+                {
+                    _camera.Add(brand, new Camera());
+                }
+            }
+            return _camera[brand];
+        }
+    }
+}
---
+++ 22_Multiton/Program.cs
@@ -0,0 +1,11 @@
+﻿using _22_Multiton;
+
+Camera camera1 = Camera.GetCamera("NIKON");
+Camera camera2 = Camera.GetCamera("NIKON");
+Camera camera3 = Camera.GetCamera("CANON");
+Camera camera4 = Camera.GetCamera("CANON");
+
+Console.WriteLine(camera1.Id);
+Console.WriteLine(camera2.Id);
+Console.WriteLine(camera3.Id);
+Console.WriteLine(camera4.Id);
